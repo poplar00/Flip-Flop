7dd66522982bb1e6b757d92eb43741d2eeb26826
Hamdan <hamdan24001@mail.unpad.ac.id>
2025-03-24 14:01:46 +0700

    feat: physics n shit + scoring

--- Scripts/Player_Controller.gd
+++ Scripts/Player_Controller.gd
@@ -1,14 +1,19 @@
 extends CharacterBody2D
 
 @export var speed = 300.0
-@export var jump_force = -800.0
+@export var jump_force = -500.0
 var IS_ALIVE : bool = true
+var SUPER : bool = false
 
 func _physics_process(delta: float) -> void:
-	if is_on_floor():
-		pass
-	else:
+	if !is_on_floor():
 		velocity += get_gravity() * delta
-	
+	if SUPER:
+		speed = 400
 	if position.y > 3000:
 		IS_ALIVE = false
+
+
+func _on_timer_timeout() -> void:
+	speed = 300.0
+	SUPER = false
